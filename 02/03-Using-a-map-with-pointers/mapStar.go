e7345c2cb4875b4f465916e602927e54f2b6d643
petros <pit.trak@gmail.com>
2021-07-01 13:50:23 +0300

    Using a map with pointers

---
+++ 02/03-Using-a-map-with-pointers/mapStar.go
@@ -0,0 +1,23 @@
+package main
+
+import "runtime"
+
+func main() {
+
+	var N = 40000000
+	myMap := make(map[int]*int)
+
+	// for loop is used for putting all values into structures that are
+	// stored in the map
+	for i := 0; i < N; i++ {
+		value := int(i)
+		myMap[value] = &value
+	}
+
+	runtime.GC()
+
+	// - = myMap[0] is used for preventing the garbage collector from collectin the map
+	// variable too early, as it is not referenced or used outside of the for loop.
+	_ = myMap[0]
+
+}
