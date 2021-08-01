d9aea148a7900551e1abe4b55d8d48ec5cf8468e
petrostrak <pit.trak@gmail.com>
2021-08-01 16:53:41 +0300

    The sync.Mutex type

---
+++ 10/09-Shared-memory-and-shared-variables/01-The-sync-Mutex-type/mutex.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+// type Mutex struct {
+// 	state int32
+// 	sema  uint32
+// }
+
+var (
+	m  sync.Mutex
+	v1 int
+)
+
+func change(i int) {
+	m.Lock()
+	time.Sleep(time.Second)
+	v1 = v1 + 1
+	if v1%10 == 0 {
+		v1 = v1 - 10*i
+	}
+	m.Unlock()
+
+}
+
+func read() int {
+	m.Lock()
+	a := v1
+	m.Unlock()
+	return a
+}
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("Please give me an integer")
+		return
+	}
+
+	numGR, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var waitGroup sync.WaitGroup
+
+	fmt.Printf("%d", read())
+	for i := 0; i < numGR; i++ {
+		waitGroup.Add(1)
+		go func(x int) {
+			defer waitGroup.Done()
+			change(x)
+			fmt.Printf("-> %d", read())
+		}(i)
+	}
+
+	waitGroup.Wait()
+	fmt.Printf("-> %d\n", read())
+
+}
