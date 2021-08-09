d0a8b469450232b0cd1f8b97d8f2300608b422fc
petrostrak <pit.trak@gmail.com>
2021-08-09 10:46:50 +0300

    HTTP tracing

---
+++ 11/09-HTTP-tracing/httpTrace.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptrace"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: URL\n")
+		return
+	}
+
+	URL := os.Args[1]
+	client := http.Client{}
+
+	req, _ := http.NewRequest("GET", URL, nil)
+	trace := &httptrace.ClientTrace{
+		GotFirstResponseByte: func() {
+			fmt.Println("First response byte")
+		},
+		GotConn: func(gci httptrace.GotConnInfo) {
+			fmt.Printf("Got Conn: %+v\n", gci)
+		},
+		DNSDone: func(di httptrace.DNSDoneInfo) {
+			fmt.Printf("DNS Info: %+v\n", di)
+		},
+		ConnectStart: func(network, addr string) {
+			fmt.Println("Dial start")
+		},
+		ConnectDone: func(network, addr string, err error) {
+			fmt.Println("Dial done")
+		},
+		WroteHeaders: func() {
+			fmt.Println("Wrote headers")
+		},
+	}
+
+	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+	fmt.Println("Requesting data from server")
+	if _, err := http.DefaultTransport.RoundTrip(req); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	io.Copy(os.Stdout, response.Body)
+
+}
