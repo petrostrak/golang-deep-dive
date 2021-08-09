0dbf213e0e7b05b893b6fe9742bd01318e15eaf4
petrostrak <pit.trak@gmail.com>
2021-08-09 22:14:15 +0300

    Testing HTTP handlers

---
+++ 11/10-Testing-HTTP-handlers/testWWW.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func CheckStatusOK(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, `Fine!`)
+}
+
+func StatusNotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func MyHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Serving: %s\n", r.URL.Path)
+	fmt.Printf("Served: %s\n", r.Host)
+}
+
+func main() {
+
+	PORT := ":8080"
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Using default port number: ", PORT)
+	} else {
+		PORT = ":" + arguments[1]
+	}
+
+	http.HandleFunc("/CheckStatusOK", CheckStatusOK)
+	http.HandleFunc("/StatusNotFound", StatusNotFound)
+	http.HandleFunc("/", MyHandler)
+
+	if err := http.ListenAndServe(PORT, nil); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
