d16866d55a26684ef377b7610d82adfd0a508186
petrostrak <pit.trak@gmail.com>
2021-08-20 19:29:02 +0300

    Rotate image by 90degrees clockwise

--- 13/01-Rotate-Image/rotateImage.go
+++ 13/01-Rotate-Image/rotateImage.go
@@ -9,10 +9,19 @@ func main() {
 	img := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
 	N := len(img)
 
+	// [1 2 3]
+	// [4 5 6]
+	// [7 8 9]
+	// Initial array
 	for i := 0; i < N; i++ {
 		fmt.Println(img[i])
 	}
 
+	// [1 4 7]
+	// [2 5 8]
+	// [3 6 9]
+	// This nested loop swaps the diagonal values
+	// 2-4, 3-7 and 6-8
 	for i := 0; i < N; i++ {
 		for j := i; j < N; j++ {
 			temp := img[i][j]
@@ -26,6 +35,11 @@ func main() {
 		fmt.Println(img[i])
 	}
 
+	// [7 4 1]
+	// [8 5 2]
+	// [9 6 3]
+	// We traveres through the half array
+	// swapping the values at the eadges
 	for i := 0; i < N; i++ {
 		for j := 0; j < N/2; j++ {
 			temp := img[i][j]
