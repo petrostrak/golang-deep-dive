f8c5b40766f2444799b039390d998085058d187b
petrostrak <pit.trak@gmail.com>
2021-08-20 12:32:26 +0300

    Sum of two O(2n)

--- 13/04-Sum-Of-Two/sumOfTwo.go
+++ 13/04-Sum-Of-Two/sumOfTwo.go
@@ -16,14 +16,49 @@ func main() {
 	}
 
 	sumOfTwo(a, b, c)
+	sumOfTwoButBetter(a, b, c)
 
 }
 
+// O(2n) Complexity
+func sumOfTwoButBetter(a, b []int, c int) bool {
+
+	// we create a map to calculate and store the value we need in order
+	// to have an addition.
+	//
+	// Interacting with maps have a constant time complexity
+	complementArr := make(map[int]int)
+
+	for i := 0; i < len(a); i++ {
+
+		// for every index of the given array, we calculate the value that
+		// we need and store it into the map
+		complementArr[i] = c - a[i]
+	}
+
+	for j := 0; j < len(b); j++ {
+
+		// then we check if there is a value from the second array
+		// that matches for the addition.
+		if complementArr[j] == b[j] {
+			fmt.Println("True")
+			return true
+		}
+	}
+
+	fmt.Println("False")
+	return false
+}
+
+// O(n^2) Complexity
 func sumOfTwo(a, b []int, c int) bool {
 	for i := 0; i < len(a); i++ {
 
 		// we subtract from the given integer the index
 		// so that we can find what we need.
+		//
+		// for example, if c = 7 and a[i] = 4 then we need
+		// the value of 3 to have a positive result.
 		neededValue := c - a[i]
 		for j := 0; j < len(b); j++ {
 			if b[i] == neededValue {
