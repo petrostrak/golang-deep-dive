a8b14339bb396fc8fb0654e1d0329248c174218e
petrostrak <pit.trak@gmail.com>
2021-08-21 15:55:08 +0300

    Products of array except self O(3n)

---
+++ 13/07-Find-Product-Except-Self/productOfArrayExceptSelf.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+// Loop through an array and calculate the product of each
+// element of the array, except self element. Do not use any
+// division.
+//
+// eg. arr := []int{1,2,3,4}
+// answer []int{24, 12, 8, 6}
+//
+// The solution would be, one loop will calculate the products
+// on the left side of the index, another one all the products
+// on the right side of the index and the last one will multiply
+// both results into the returning array.
+//
+// input =  [1,2,3,4]
+// leftProduct = [1,1,2,6]
+// rightProduct = [24,12,4,1] (filling backwords)
+// output = [24,12,8,6]
+func main() {
+
+	arr := []int{1, 2, 3, 4}
+	fmt.Println(productOfArrayExceptSelf(arr))
+
+}
+
+// O(3n) Complexity
+func productOfArrayExceptSelf(arr []int) []int {
+	result := make([]int, len(arr))
+	leftProducts := make([]int, len(arr))
+	rightProducts := make([]int, len(arr))
+
+	leftProducts[0] = 1
+	rightProducts[len(arr)-1] = 1
+
+	// this loop calculates all the products to the
+	// left of the given array.
+	for i := 1; i < len(arr); i++ {
+		leftProducts[i] = arr[i-1] * leftProducts[i-1]
+	}
+
+	// this loop calculates all the products to the
+	// right of the given array.
+	for i := len(arr) - 2; i >= 0; i-- {
+		rightProducts[i] = arr[i+1] * rightProducts[i+1]
+	}
+
+	// this loop multiplies the left and right products
+	for i := 0; i < len(arr); i++ {
+		result[i] = leftProducts[i] * rightProducts[i]
+	}
+
+	return result
+}
