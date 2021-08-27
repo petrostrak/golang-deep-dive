a3b6832256a0da4d84eec81b53f6c9e082c273e7
petros <pit.trak@gmail.com>
2021-08-27 09:51:25 +0300

    Heaps - Heapify down

--- 13/15-Heaps/binaryHeap.go
+++ 13/15-Heaps/binaryHeap.go
@@ -20,9 +20,14 @@ import "fmt"
 // process if needed.
 func main() {
 	m := &MaxHeap{}
-	myHeap := []int{10, 20, 30}
+	myHeap := []int{10, 20, 30, 15, 5, 45, 50, 87, 55, 90, 100, 44, 67, 87}
 	for _, v := range myHeap {
 		m.Insert(v)
+		fmt.Println(m)
+	}
+
+	for i := 0; i < 5; i++ {
+		m.Extract()
 		fmt.Println(m)
 	}
 }
@@ -36,6 +41,53 @@ type MaxHeap struct {
 func (h *MaxHeap) Insert(key int) {
 	h.array = append(h.array, key)
 	h.maxHeapifyUp(len(h.array) - 1)
+}
+
+// Extract returns the largest key and removes it from the heap
+func (h *MaxHeap) Extract() int {
+	extracted := h.array[0]
+	l := len(h.array) - 1
+
+	// when array is empty
+	if len(h.array) == 0 {
+		fmt.Println("cannot extract because array length is 0")
+		return -1
+	}
+
+	// take out the last index nad put it in the root
+	h.array[0] = h.array[l]
+	h.array = h.array[:l]
+
+	h.maxHeapifyDown(0)
+
+	return extracted
+}
+
+func (h *MaxHeap) maxHeapifyDown(index int) {
+	lastIndex := len(h.array) - 1
+	l, r := left(index), right(index)
+	childToCompare := 0
+
+	// while index has at least one child
+	for l <= lastIndex {
+		if l == lastIndex { // when left child is the only child
+			childToCompare = l
+		} else if h.array[l] > h.array[r] { // whenn left child is larger
+			childToCompare = l
+		} else { // when right child is larger
+			childToCompare = r
+		}
+
+		// compare array value of current index to larger child and
+		// swap if smaller
+		if h.array[index] < h.array[childToCompare] {
+			h.swap(index, childToCompare)
+			index = childToCompare
+			l, r = left(index), right(index)
+		} else {
+			return
+		}
+	}
 }
 
 // We want to swap when the current index is
