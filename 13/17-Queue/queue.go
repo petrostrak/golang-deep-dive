942a2be9d159d7d52e6d756477d97ac49fd28d8e
petrostrak <pit.trak@gmail.com>
2021-08-30 23:15:02 +0300

    Queue

---
+++ 13/17-Queue/queue.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Value int
+}
+
+// Queue is the basic FIFO queue based on a circular list
+// that resizes as needed
+type Queue struct {
+	nodes []*Node
+	size  int
+	head  int
+	tail  int
+	count int
+}
+
+// NewQueue returns a new queue with the given initial size
+func NewQueue(size int) *Queue {
+	return &Queue{
+		nodes: make([]*Node, size),
+		size:  size,
+	}
+}
+
+// Push adds a node to the queue
+func (q *Queue) Push(n *Node) {
+	if q.head == q.tail && q.count > 0 {
+		nodes := make([]*Node, len(q.nodes)+q.size)
+		copy(nodes, q.nodes[q.head:])
+		copy(nodes[len(q.nodes)-q.head:], q.nodes[:q.head])
+		q.head = 0
+		q.tail = len(q.nodes)
+		q.nodes = nodes
+	}
+
+	q.nodes[q.tail] = n
+	q.tail = (q.tail + 1) % len(q.nodes)
+	q.count++
+}
+
+// Pop removes and returns a node from the queu in the first to last
+// order
+func (q *Queue) Pop() *Node {
+	if q.count == 0 {
+		return nil
+	}
+	node := q.nodes[q.head]
+	q.head = (q.tail + 1) % len(q.nodes)
+	q.count--
+	return node
+}
+
+func main() {
+
+	q := NewQueue(1)
+	q.Push(&Node{2})
+	q.Push(&Node{4})
+	q.Push(&Node{6})
+	q.Push(&Node{8})
+	q.Push(&Node{10})
+	fmt.Println(q.Pop(), q.Pop(), q.Pop(), q.Pop())
+
+}
