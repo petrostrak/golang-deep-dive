19d7f9c015fa8361491ccf10aa053b84b637ebe1
petros <pit.trak@gmail.com>
2021-09-07 20:17:13 +0300

    Trie insert method

--- 13/27-Trie-data-structure/trie.go
+++ 13/27-Trie-data-structure/trie.go
@@ -22,7 +22,19 @@ func initTrie() *Trie {
 	return &Trie{root: &Node{}}
 }
 
-// Insert
+// Insert will take in a word and add it to the trie
+func (t *Trie) Insert(w string) {
+	wordLength := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			currentNode.children[charIndex] = &Node{}
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	currentNode.isEnd = true
+}
 
 // Search
 
